38353199299a6b67deef69139331689e663fe735
Gerardo <romero.gerardo.ds@gmail.com>
2026-05-17 23:00:36 -0300

    Agregué README.md y ajustes en server.js

---
+++ BackEnd/README.md
@@ -0,0 +1,12 @@
+# Gestor de Cursos Online
+
+Proyecto MVP desarrollado para la materia **Ingeniería de Software**, aplicando patrones de diseño y arquitectura (MVC, Repository, Singleton) y validado con pruebas en equipo.
+
+---
+
+## 🚀 Instalación
+
+1. Clonar el repositorio:
+   ```bash
+   git clone <URL-del-repo>
+   cd TP-MVP/BackEnd
--- BackEnd/server.js
+++ BackEnd/server.js
@@ -5,6 +5,7 @@ app.use(express.json())
 const cursoController = require("./controllers/cursoController")
 const alumnoController = require("./controllers/alumnoController")
 const inscripcionController = require("./controllers/inscripcionController")
+const profesorController = require("./controllers/profesorController")
 
 app.post("/cursos", cursoController.crearCurso)
 app.get("/cursos", cursoController.listarCursos)
@@ -12,6 +13,8 @@ app.post("/alumnos", alumnoController.crearAlumno)
 app.get("/alumnos", alumnoController.listarAlumnos)
 app.post("/inscripciones", inscripcionController.inscribirAlumno)
 app.get("/inscripciones", inscripcionController.listarInscripciones)
+app.post("/profesores", profesorController.crearProfesor)
+app.get("/profesores", profesorController.listarProfesores)
 
 app.listen(3000, () => {
     console.log("Servidor en http://localhost:3000");
